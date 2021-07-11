ac2986934656806ed4f917df835308ec4240c93c
juandalopez117 <77028323+juandalopez117@users.noreply.github.com>
2021-07-10 19:41:58 -0500

    Update Ant_Colony.py

--- Ant_Colony.py
+++ Ant_Colony.py
@@ -6,19 +6,7 @@ class AntColony(object):
 
     def __init__(self, start, end, distances,  
                  n_ants, n_best, n_iterations, decay, alpha=1, beta=1):
-        """
-        Args:
-            distances (2D numpy.array): Square matrix of distances. Diagonal is assumed to be np.inf.
-            n_ants (int): Number of ants running per iteration
-            n_best (int): Number of best ants who deposit pheromone
-            n_iteration (int): Number of iterations
-            decay (float): Rate it which pheromone decays. The pheromone value is multiplied by decay, so 0.95 will lead to decay, 0.5 to much faster decay.
-            alpha (int or float): exponenet on pheromone, higher alpha gives pheromone more weight. Default=1
-            beta (int or float): exponent on distance, higher beta give distance more weight. Default=1
-
-        Example:
-            ant_colony = AntColony(german_distances, 100, 20, 2000, 0.95, alpha=1, beta=2)          
-        """
+     
         self.distances  = distances
         self.pheromone = np.ones(self.distances.shape) / len(distances)
         self.all_inds = range(len(distances))
